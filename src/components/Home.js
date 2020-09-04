cb47ad38e2c6d9f3914bf55b1640803e7ed029e3
Rohan Chaudhary <rohanc8.rc@gmail.com>
2020-09-05 02:03:39 +0530

    just starting

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home(props) {
+    return (
+        <div>
+            <h1>home thing</h1>
+            
+        </div>
+    )
+}
---
+++ src/components/maincomp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Home from './Home'
+import Menu from './Menu'
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+
+class maincomp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default maincomp
