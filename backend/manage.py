c171e4b93ff0026d55d9d15217d8551dcfeee6a6
Mohit Gupta <43778481+mohitrjaggarwal@users.noreply.github.com>
2020-10-17 18:58:09 +0530

    Update manage.py
    made changes for efficiency

--- backend/manage.py
+++ backend/manage.py
@@ -19,3 +19,41 @@ def main():
 
 if __name__ == '__main__':
     main()
+n = int(input("no .of enteries:"))
+list = []
+dict = {'O': "Orthopedics",'P' : "Pediatrics",'E': "ENT"}
+O =0
+P =0
+E = 0
+for i in range(2*n):
+    if(i%2 !=0):
+        entry = str(input("enter speciality"))
+        if(entry == 'O'):
+            O = O + 1
+        elif(entry == 'P'):
+            P = P + 1
+        else:
+             E = E+1
+    else:
+        entry = int(input("id no."))
+
+    list.append(entry)
+
+#comparing values
+if(O> P and O > E):
+    r = 'O'
+elif(E> P and E > O):
+    r = 'E'
+else:
+    r = 'P'
+print(dict[r])
+
+
+
+
+
+
+
+
+
+
